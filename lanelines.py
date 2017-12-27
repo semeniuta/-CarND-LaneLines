e728dd67dd214b51814b93e2830deff41082ce46
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2017-12-27 18:01:15 +0100

    Added functionality of extending lines, included it in the pipeline, improved the define_lanes_region logic

--- lanelines.py
+++ lanelines.py
@@ -13,14 +13,13 @@ def h2e(x):
     return x[:2] / x[2]
 
 
-def define_lanes_region(image, x_from=450, x_to=518, y_lim=317, left_offset=50, right_offset=0):
+def define_lanes_region(n_rows, n_cols, x_from=450, x_to=518, y_lim=317, left_offset=50, right_offset=0):
 
-    y_hi, x_hi = image.shape[:2]
     vertices = np.array([[
         [x_from, y_lim],
         [x_to, y_lim],
-        [x_hi-right_offset, y_hi],
-        [left_offset, y_hi],
+        [n_cols-right_offset, n_rows],
+        [left_offset, n_rows],
     ]], dtype=np.int32)
 
     return vertices
@@ -60,6 +59,36 @@ def line_vector_from_opencv_points(line):
 
     x1, y1, x2, y2 = line
     return np.cross([x1, y1, 1], [x2, y2, 1])
+
+
+def extend_lane_lines(lines, y_const_0, y_const_1):
+
+    n = len(lines)
+
+    res = np.zeros((n, 4), dtype=np.int32)
+
+    line_y0 = line_vector_constant_y(y_const_0)
+    line_y1 = line_vector_constant_y(y_const_1)
+
+    for i in range(n):
+
+        line = line_vector_from_opencv_points(lines[i, :])
+
+        intersection_0 = h2e(np.cross(line, line_y0))
+        intersection_1 = h2e(np.cross(line, line_y1))
+
+        res[i, :2] = intersection_0
+        res[i, 2:] = intersection_1
+
+    return res
+
+
+def extend_lane_lines_grouped_by_slopes(lines, slopes, y_const_0, y_const_1):
+
+    lines_left = extend_lane_lines(lines[slopes < 0], y_const_0, y_const_1)
+    lines_right = extend_lane_lines(lines[slopes > 0], y_const_0, y_const_1)
+
+    return lines_left, lines_right
 
 
 def draw_lines_on_image(canvas_im, lines, color=[255, 0, 0], thickness=2):

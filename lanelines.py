2bca6d6eec111d3a853ace426f3a7f9d18736016
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2017-12-28 12:31:05 +0100

    First visualization of the detected lane lines

--- lanelines.py
+++ lanelines.py
@@ -107,6 +107,13 @@ def draw_lines_on_image(canvas_im, lines, color=[255, 0, 0], thickness=2):
     for i in range(lines.shape[0]):
         draw_line(canvas_im, lines[i, :], color, thickness)
 
+def plot_line(line, **kvargs):
+
+    xs = [line[0], line[2]]
+    ys = [line[1], line[3]]
+
+    plt.plot(xs, ys, '-', **kvargs)
+
 
 def plot_homogeneous_line_vector(vec, x_from, x_to, **kvargs):
 
